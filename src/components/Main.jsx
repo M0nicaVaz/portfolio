cfe1cfdcbd9e284670afdfdf792eca1d5422bfd0
Monica Vaz <contatomonicavaz@gmail.com>
2022-07-24 13:54:47 -0300

    fix container margin

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 export function Main({ children }) {
   return (
-    <main className="grid px-12 w-screen mt-10 lg:mt-0 lg:h-[calc(100vh-72px)] lg:place-content-center">
+    <main className="grid px-12 w-screen mt-20 lg:mt-0 lg:h-[calc(100vh-72px)] lg:place-content-center">
       {children}
     </main>
   );
